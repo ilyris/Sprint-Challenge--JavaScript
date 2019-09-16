28777b0ad101673f7850d0d7aaec4a15df42c380
Dylan Sieren <dylan.sieren01@gmail.com>
2019-09-15 19:42:47 -0500

    finished the callbacks and closure assignment

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,26 +1,6 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
 
-// function CuboidMaker(attributes) {
-//     this.length = attributes.length;
-//     this.width = attributes.width;
-//     this.height = attributes.height;
-//   }
-
-//   CuboidMaker.prototype.getVolume = function() {
-//     return this.length * this.width * this.height;
-//   }
-
-//   CuboidMaker.prototype.surfaceArea = function() {
-//     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-//   }
-  
-//   const cuboid = new CuboidMaker({
-//     length: 4,
-//     width: 5,
-//     height: 5
-//   });
-
 class CuboidMakerRefactored {
     constructor(length, width, height) {
         this.length = length;
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,27 +6,42 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a, b, callBack) {
+  return callBack(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b) {
+return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation:
 
+// nestedFunction doesn't have access to internal,  since this is the case; nestedFunction is going to keep going up scopes untill it finds the reference to 
+// the variable "internal" which will allow the function "nestedFunction" to use the variable internal.
 
 const external = "I'm outside the function";
 
